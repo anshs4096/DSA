2b101d4d6183cd61c6b9b3574ed07b2583c0721f
Anshuman Singh <anshumansingh891700@gmail.com>
2026-09-09 01:26:51 +0530

    Time: 35 ms (44.32%), Space: 47 MB (59.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,7 +6,7 @@ class Solution {
                 if(nums[i]+nums[j]==target){
                     ans[0]=i;
                     ans[1]=j;
-                    return ans;
+                    break;
                 }
             }
         }
